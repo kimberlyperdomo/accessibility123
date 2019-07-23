0b24332249a92a7cdbebaf827e3cc1e1b5d891c0
kimberlyperdomo <53236396+kimberlyperdomo@users.noreply.github.com>
2019-07-23 15:44:36 -0400

    Add files via upload

---
+++ office.js
@@ -0,0 +1,10 @@
+function hide() {
+  var myGif = document.getElementById("2003");
+  myGif.style.display = "none";
+}
+
+function show() {
+  var myGif = document.getElementById("2003");
+  myGif.style.display = "block";
+  
+}
